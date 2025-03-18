7f4006b9eca5b4ac47c42eb658974168a1951c01
Devanshu-pal-github <devanshu4943@gmail.com>
2025-03-18 15:45:53 +0530

    PropDrilling Tutorial done

--- src/Components/NinthTutorial/PropDrilling.jsx
+++ src/Components/NinthTutorial/PropDrilling.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
 
 const PropDrilling = () => {
+  const name = "Devanshu";
   return (
-    <div>PropDrilling</div>
-  )
-}
+    <div className="prop-drilling">
+      <h1>PropDrilling</h1>
+      <ComponentA name={name} />
+      <ComponentB />
+      <ComponentC />
+    </div>
+  );
+};
 
-export default PropDrilling
+export default PropDrilling;
